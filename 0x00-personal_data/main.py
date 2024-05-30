060d38dda02df181ad8db3a2c0354695add4df1a
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-05-30 13:04:58 +0000

    check valid password

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -40,7 +40,9 @@ cursor.close()
 db.close()
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
